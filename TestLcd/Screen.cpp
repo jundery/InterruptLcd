039b4c885f60c8420687390a18d20624f9bddd66
jundery <jundery@gmail.com>
2011-09-15 21:40:16 -0600

    Screen code
    First cut of static screen code

---
+++ TestLcd/Screen.cpp
@@ -0,0 +1,59 @@
+#include "Screen.h"
+#include <string.h>
+
+Screen::Screen()
+{
+    memset(buffer, ' ', LCD_ROWS * LCD_COLS);
+    pCurrent = buffer;
+}
+
+Screen::Screen(char* baseScreen)
+{
+    for (int row = 0; row < LCD_ROWS; ++row)
+    {
+        SetCursor(0, row);
+        Print(baseScreen + row * LCD_COLS, LCD_COLS);
+    }
+    pCurrent = buffer;
+}
+
+void Screen::SetCursor(int col, int row)
+{
+    switch (row)
+    {
+        case 0:
+            pCurrent = buffer + col;
+            break;
+            
+        case 1:
+            pCurrent = buffer + 2 * LCD_COLS + col;
+            break;
+            
+        case 2:
+            pCurrent = buffer + LCD_COLS + col;
+            break;
+            
+        case 3:
+            pCurrent = buffer + 3 * LCD_COLS + col;
+            break;
+    }
+}
+
+void Screen::Print(char *row)
+{
+    while (*row)
+    {
+        *pCurrent++ = *row++;
+    }
+}
+
+void Screen::Print(char *row, int count)
+{
+    
+    while (*row && count)
+    {
+        *pCurrent++ = *row++;
+        --count;
+    }
+}
+
