cbb2affe151889f10ec6e609840bae375eb34111
jundery <jundery@gmail.com>
2011-09-18 13:50:28 -0600

    Add floating point printing

--- TestLcd/Screen.cpp
+++ TestLcd/Screen.cpp
@@ -21,21 +21,21 @@ void Screen::SetCursor(int col, int row)
 {
     switch (row)
     {
-        case 0:
-            pCurrent = buffer + col;
-            break;
-            
-        case 1:
-            pCurrent = buffer + 2 * LCD_COLS + col;
-            break;
-            
-        case 2:
-            pCurrent = buffer + LCD_COLS + col;
-            break;
-            
-        case 3:
-            pCurrent = buffer + 3 * LCD_COLS + col;
-            break;
+    case 0:
+        pCurrent = buffer + col;
+        break;
+
+    case 1:
+        pCurrent = buffer + 2 * LCD_COLS + col;
+        break;
+
+    case 2:
+        pCurrent = buffer + LCD_COLS + col;
+        break;
+
+    case 3:
+        pCurrent = buffer + 3 * LCD_COLS + col;
+        break;
     }
 }
 
@@ -49,7 +49,7 @@ void Screen::Print(char *row)
 
 void Screen::Print(char *row, int count)
 {
-    
+
     while (*row && count)
     {
         *pCurrent++ = *row++;
@@ -59,6 +59,93 @@ void Screen::Print(char *row, int count)
 
 bool Screen::Display()
 {
-    return lcdWriteBuffer(buffer);
+    return lcdWriteBuffer((uint8_t*)buffer);
 }
+
+// Print float with +123.4 format
+void Screen::PrintFloat31(float value)
+{
+    int total;
+    int digit;
+    if (value >= 0)
+    {
+        *pCurrent++ = '+';
+        value *= 0.01;
+    }
+    else
+    {
+        *pCurrent++ = '-';
+        value *= -0.01;
+    }
+
+    digit = value;  
+
+    *pCurrent++ = digit + '0'; 
+    total = digit * 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = digit + '0';
+    total += digit;
+    total *= 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = digit + '0'; 
+    total += digit;
+    total *= 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = '.';
+
+    *pCurrent++ = digit + '0';
+}
+
+//  Print float with +1234.5 format
+void Screen::PrintFloat41(float value)
+{
+    int total;
+    int digit;
+    if (value >= 0)
+    {
+        *pCurrent++ = '+';
+        value *= 0.001;
+    }
+    else
+    {
+        *pCurrent++ = '-';
+        value *= -0.001;
+    }
+
+    digit = value;  
+
+    *pCurrent++ = digit + '0'; 
+    total = digit * 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = digit + '0';
+    total += digit;
+    total *= 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = digit + '0';
+    total += digit;
+    total *= 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = digit + '0'; 
+    total += digit;
+    total *= 10; 
+    value *= 10.0;
+    digit = (int)value - total;
+
+    *pCurrent++ = '.';
+
+    *pCurrent++ = digit + '0';
+}
+
 
--- TestLcd/Screen.h
+++ TestLcd/Screen.h
@@ -8,17 +8,23 @@ class Screen
 public:
     Screen();
     Screen(char* baseScreen);
-    
+
+    char* GetCursor() const { return pCurrent; }
+    void SetCursor(char *pCursor) { pCurrent = pCursor; }
     void SetCursor(int col, int row);
-    
+
     void Print(char* row);
     void Print(char* row, int count);
-    
+
+    void PrintFloat31(float value);
+    void PrintFloat41(float value);
+
     bool Display();
-    
+
 private:
-    uint8_t buffer[LCD_ROWS * LCD_COLS];
-    uint8_t* pCurrent;
+    char buffer[LCD_ROWS * LCD_COLS];
+    char* pCurrent;
 };
 
 #endif
+
